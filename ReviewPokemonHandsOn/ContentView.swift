8990382d895aee453af2e9e944ab69dc6688b266
yuya-3110 <ysuu45143745@gmail.com>
2025-03-21 18:55:05 +0900

    Initial Commit

---
+++ ReviewPokemonHandsOn/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ReviewPokemonHandsOn
+//  
+//  Created by Yuchinante on 2025/03/21
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReviewPokemonHandsOn/ReviewPokemonHandsOnApp.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewPokemonHandsOnApp.swift
+//  ReviewPokemonHandsOn
+//  
+//  Created by Yuchinante on 2025/03/21
+//  
+//
+
+import SwiftUI
+
+@main
+struct ReviewPokemonHandsOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
